1835e18737200d7f28bcbeebf9b080eafc79dfc5
A231871 <nguyentho0933872322@gmail.com>
2026-01-26 19:44:15 +0700

    add bai2-full

--- bai2/src/main/java/com/example/bai2/controller/HomeController.java
+++ bai2/src/main/java/com/example/bai2/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.bai2;
+package com.example.bai2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
